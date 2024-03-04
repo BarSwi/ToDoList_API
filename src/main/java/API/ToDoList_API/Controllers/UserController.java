62e181da3ee42b7e58b4cd160397350a5583384a
user <bartosz.switala.01@gmail.com>
2024-03-05 00:00:05 +0100

    Set up PostRequest for authentication

--- src/main/java/API/ToDoList_API/Controllers/UserController.java
+++ src/main/java/API/ToDoList_API/Controllers/UserController.java
@@ -5,10 +5,12 @@ import API.ToDoList_API.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -17,10 +19,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/test/{username}")
-    public ResponseEntity<?> test(@PathVariable String username){
-        String password = "CqzjNqU7WXMHQy7BL2whUqlB9aqG4WR1O3XJ90d40zg=";
+    @PostMapping("/authentication")
+    public ResponseEntity<?> test(@RequestBody Map<String, String> request){
+        String username = request.get("username");
+        String password = request.get("password");
         User authenticatedUser = userService.Authenticate(username, password).orElse(null);
+
+
 
         if (authenticatedUser != null) {
             // Authentication successful, return the user or any required information
